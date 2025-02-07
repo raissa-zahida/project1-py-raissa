f9e050d7a0ee2667055b92f53169c86b34687ddb
raissa-zahida <raissa.zahida@student.alhazen.academy>
2025-02-07 11:44:33 +0700

    Add files via upload

---
+++ introduction.py
@@ -0,0 +1,27 @@
+print("hello world")
+print("Selamat datang")
+
+print("hello",end=" ")
+print("semuanya....")
+
+name="Raissa"
+age= 17
+print('hello',"im",name,",",age,"years old")
+print("hello my name is "+ name + "," + "im " + str(age) + " y/o" )
+print(f"hello {name}, {age} years old")
+ 
+nama= input("enter your name: ")
+email= input("enter your email: ")
+
+
+print(f"thanks {nama} for registering. we will send an email to {email}")
+
+number = 25 #ini variable tipe data integer
+pi = 3.14 #ini variable tipe data float
+
+#ini variable dengan data complex
+a = 45 + 89.5j
+
+print(type(number))
+print(type(pi))
+print(type(a))
