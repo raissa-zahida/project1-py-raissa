031cd5e8cef7445af92b096aacd6b8f6e639f4b7
raissa-zahida <raissa.zahida@student.alhazen.academy>
2025-08-22 12:56:20 +0700

    Add files via upload

---
+++ Lambda.py
@@ -0,0 +1,11 @@
+lambda name: print("Hello, nice to meet you", name)
+SayHello = lambda name: print("Hello, nice to meet you", name)
+SayHello ("Raissa")
+
+lambda name1: print("what are you up to? need some", name1,"help")
+OfferHelp = lambda name1: print("what are you up to? need some", name1,"help")
+OfferHelp ("supr")
+
+(lambda name2   : print(f"Okay {name2},follow me in this journey"))("explorer")
+offtogo = (lambda name2 : print(f"Okay {name2},follow me in this journey"))("explorer")
+print(offtogo)
